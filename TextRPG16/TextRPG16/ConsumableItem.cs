40f7b11a0e0f127d871427b7cb45b6c6f52e21e7
Lookandme <kgg9386@gmail.com>
2024-09-27 15:01:02 +0900

    소모성 아이템 생성자 생성

--- TextRPG16/TextRPG16/ConsumableItem.cs
+++ TextRPG16/TextRPG16/ConsumableItem.cs
@@ -5,8 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TextRPG16
-{
-    internal class ConsumableItem
+{        //소모성 아이템 클래스
+         // 회복 시스템 구현
+    public class ConsumableItem
     {
+        public ConsumableItem() 
+        {
+            
+
+        }
     }
 }

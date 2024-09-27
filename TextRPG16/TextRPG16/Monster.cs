b22436a7698cdea75285e7d4ec56ae44d9dc5c70
woollim <137277221+woollim23@users.noreply.github.com>
2024-09-27 16:53:54 +0900

    몬스터클래스 프로퍼티 접근 지정자 수정

--- TextRPG16/TextRPG16/Monster.cs
+++ TextRPG16/TextRPG16/Monster.cs
@@ -1,6 +1,62 @@
 namespace TextRPG16
 {
     class Monster : ICharacter{
-        
+        // ------------------ 캐릭터 인터페이스 공통 ------------------
+        string _name = null!; // 이름  // - 자식도 수정가능
+        int _level;
+        string _tribe = "Monster"; // 종족
+        int _HP; // 현재 피통
+        int _fullHP; // 최대피통
+        int _attackDamage; // 공격력
+        bool _isDead = false; // 생존여부
+
+        public String Name { get { return _name; } protected set { _name = value; } }
+        public int Level { get { return _level; } protected set { _level = value; } } // 레벨
+        public String Tribe { get { return _tribe; } private set { _tribe = value; } }
+        public int HP { get { return _HP; } set { _HP = value; } }
+        public int FullHP { get { return _fullHP; } set { _fullHP = value; } }
+        public int AttackDamage { get { return _attackDamage; } set { _attackDamage = value; } }
+        public bool IsDead { get { return _isDead; } set { _isDead = value; } }
+
+        public void TakeDamage(int damage)
+        {
+
+        }
+
+
+    }
+
+    class Dookie: Monster
+    {
+        public Dookie()
+        {
+            this.Name = "두키";
+            this.FullHP = 10;
+
+        }
+    }
+
+    class Slime: Monster
+    {
+        public Slime(int level)
+        {
+            this.Name = "슬라임";
+        }
+    }
+
+    class Leejinho: Monster
+    {
+        public Leejinho(int level)
+        {
+            this.Name = "이진호";
+        }
+    }
+
+    class Dragon: Monster
+    {
+        public Dragon(int level)
+        {
+            this.Name = "드래곤";
+        }
     }
 }

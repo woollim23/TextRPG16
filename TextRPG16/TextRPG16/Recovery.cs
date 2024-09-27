198477238605752353bb862494469c3f0dbd0087
Lookandme <kgg9386@gmail.com>
2024-09-28 01:16:36 +0900

    회복하기 기능 구현 조건 체력과 마나 별개로 회복되게 수정

--- TextRPG16/TextRPG16/Recovery.cs
+++ TextRPG16/TextRPG16/Recovery.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextRPG16
+﻿namespace TextRPG16
 {
     public class Recovery
     {
-        public Recovery() 
+        public Recovery()
         {
-            
+
         }
 
         public void Rest(User user)
         {
-            while(user.HP < user.FullHP || user.MP < user.FullMP)
-            {
-                user.HP++;
-                user.MP++;
-                Console.WriteLine($"회복중입니다. 현재 체력:{user.HP} 현재 마나:{user.MP}");
-                if(user.HP == user.FullHP && user.MP == user.FullMP)
-                    break;
-            }
+            while (user.HP < user.FullHP) { user.HP++; }  // 마을에서 휴식하는것과 같은방식
+            while (user.MP < user.FullMP) { user.MP++; }  // 체력이랑 마나랑 별개의 통을 가지고 있으니까 반복문 두개로 나눔
+            Console.WriteLine($"회복중입니다. 현재 체력:{user.HP} 현재 마나:{user.MP}");
             Console.Clear();
             Console.WriteLine("회복이 완료되었습니다");
         }
-
     }
 }
+
+
+
+

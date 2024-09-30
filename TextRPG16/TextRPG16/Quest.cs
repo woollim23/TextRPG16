7453c4e7fa943753a8523071d65147b918da60f0
ojs0124 <ojs_0124_@naver.com>
2024-09-30 15:23:36 +0900

    Update Quest.cs

--- TextRPG16/TextRPG16/Quest.cs
+++ TextRPG16/TextRPG16/Quest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextRPG16
+﻿namespace TextRPG16
 {
     //List<Quest> quests = new List<Quest>();
 
@@ -13,17 +7,39 @@ namespace TextRPG16
         enum QuestType { Equip, Growth, Hunt };
         string name = null!;
         string context = null!;
-        
-        bool equip = false;
-        int lvUp = 0;
-        int mobCnt = 0;
+
+        public bool isEquip = false;
+
+        public int lvUp = 0;
+
+        public string mobName = null!;
+        public int totalMob = 0;
+        public int mobCnt = 0;
+
+        bool isAccept = false;
         bool isClear = false;
 
         Item amends = null!;
 
-        public Quest()
+        public Quest(int questType, string name, string context, Item amends)
         {
+            this.name = name;
+            this.context = context;
+            this.amends = amends;
 
+            Random random = new Random();
+
+            switch ((QuestType)questType)
+            {
+                case QuestType.Equip:
+                    break;
+                case QuestType.Growth:
+                    break;
+                case QuestType.Hunt:
+                    //mobName = null;
+                    totalMob = random.Next(1, 6);
+                    break;
+            }
         }
 
         public int DisplayQuest()
@@ -34,12 +50,34 @@ namespace TextRPG16
             Console.WriteLine(name);
             Console.WriteLine();
 
-            // 퀘스트 클리어 조건
-            // 클리어 보상
-
-            Console.WriteLine("1. 수락");
-            Console.WriteLine("2. 거절");
+            Console.WriteLine(context);
             Console.WriteLine();
+
+            // 퀘스트 클리어 조건
+            Console.WriteLine($"ex) {mobName} {totalMob}마리 처치 ({mobCnt}/{totalMob})");
+
+            // 클리어 보상
+            Console.WriteLine("- 보상 -");
+            Console.WriteLine(amends);
+
+            if (!isAccept)
+            {
+                Console.WriteLine("1. 퀘스트 수락");
+                Console.WriteLine("2. 퀘스트 거절");
+                Console.WriteLine();
+            }
+            else if (isAccept && !isClear)
+            {
+                Console.WriteLine("1. 퀘스트 포기하기");
+                Console.WriteLine("2. 돌아가기");
+                Console.WriteLine();
+            }
+            else if (isAccept && isClear)
+            {
+                Console.WriteLine("1. 퀘스트 보상받기");
+                Console.WriteLine("2. 돌아가기");
+                Console.WriteLine();
+            }
 
             Console.WriteLine("원하시는 행동을 입력해주세요");
             Console.Write(">>> ");
